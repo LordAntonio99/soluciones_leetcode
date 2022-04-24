18013cfb48c5eec52cc069649aed240cfb792836
LordAntonio99 <tonicintora@gmail.com>
2022-04-24 13:51:19 +0200

    ejercicio

--- reformat_date.js
+++ reformat_date.js
@@ -1,9 +1,27 @@
+const meses = {
+  Jan: "01",
+  Feb: "02",
+  Mar: "03",
+  Apr: "04",
+  May: "05",
+  Jun: "06",
+  Jul: "07",
+  Aug: "08",
+  Sep: "09",
+  Oct: "10",
+  Nov: "11",
+  Dec: "12",
+};
+
 /**
  * @param {string} date
  * @return {string}
  */
- var reformatDate = function(date) {
-    
+var reformatDate = function (date) {
+  const [dia, mes, ano] = date.split(" ");
+  const diaNum = parseInt(dia, 10);
+  const diaFinal = diaNum < 10 ? "0" + diaNum : diaNum;
+  return ano + "-" + meses[mes] + "-" + diaFinal;
 };
 
-module.exports = reformatDate
+module.exports = reformatDate;
